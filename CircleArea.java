80192df37b728876140369efd847ea5871e4a995
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-06 18:16:52 +0900

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,26 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+
+		Double radius = 5.6;
+		final double PI = 3.14;
+		Double area = radius * radius * PI;
+
+		System.out.println(area);
+
+	}
+
+}
+
+/*이클립스로 java_area 프로젝트를 만들고, 
+아래의 조건으로 프로그램하시오.
+
+CircleArea.java 파일을 만들고
+
+원의 반지름 5.6 을 담는 변수 선언.
+PI 상수로 선언 
+
+원의 넓이 공식 파이알의제곱을 담는 변수 선언 하여 
+넓이를 담고 출력
+*/
