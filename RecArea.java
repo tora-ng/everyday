b72fa6afea4ce9dd3bfd99e592a2bb6eb4f40173
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-06 18:17:31 +0900

    Add files via upload

---
+++ RecArea.java
@@ -0,0 +1,27 @@
+
+public class RecArea {
+
+	public static void main(String[] args) {
+
+		int hor = 10;
+		int ver = 20;
+		int area = hor * ver;
+
+		System.out.println(area);
+
+	}
+
+}
+
+/*"이클립스로 java_area 프로젝트를 만들고, 
+아래의 조건으로 프로그램하시오.
+
+RecArea.java 파일을 만들고
+
+가로와 세로를 담는 변수 선언. 해당 변수에 임의 
+가로,세로값 할당
+
+넓이를 담는 변수 선언
+
+해당 넓이 출력
+*/
