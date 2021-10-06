24b1881e6c9435f731d03d01a2383d5618352e54
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-06 18:15:37 +0900

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,34 @@
+package java_variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+
+		int num1;
+		int num2;
+		int result;
+
+		num1 = 100;
+		num2 = 200;
+
+		result = num1 + num2;
+		System.out.println("두 수의 합은 " + result);
+
+		/*
+		 * int x = 100; int y = 200; int result = x + y; System.out.println(result);
+		 */
+
+	}
+
+}
+
+/*
+이클립스로 java_variable 프로젝트를 만들고, 
+아래의 조건으로 프로그램하시오.
+
+Variable.java 파일을 만들고
+
+100 과 200을 담는 변수 두개를 선언후,
+두수의 합의 결과를 담는 변수를 선언하여.
+해당 두수의 합을 출력하는 프로그램을 작성 하시오.
+*/
