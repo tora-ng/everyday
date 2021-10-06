4cd70d5311df06573a2fefd101e49a8d6240bcca
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-06 18:14:42 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("I am a programer");
+		System.out.println("HelloWorld");
+	}
+
+}
+
+/*
+이클립스로 HelloWorld3 프로젝트를 만들고,
+Hello.java 파일을 생성후
+아래를 출력하는 프로그램을 만들어라
+"I am a programer
+HelloWorld"
+*/
