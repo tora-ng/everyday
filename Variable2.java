e1dd11077e59d7055365301e093b465db6dffd04
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-06 18:16:12 +0900

    Add files via upload

---
+++ Variable2.java
@@ -0,0 +1,31 @@
+package java_variable;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+
+		Double num1;
+		Double num2;
+		Double result;
+
+		num1 = 3.14;
+		num2 = 5.14;
+		result = num1 * num2;
+		System.out.println(result);
+
+		/*
+		 * double x = 3.14; double y = 5.14; double result = x * y;
+		 * System.out.println(result);
+		 */
+	}
+
+}
+
+/*
+Variable2.java 파일을 만들고
+
+3.14 과 5.14을 담는 변수 두개를 선언후,
+두수의 곱의 결과를 담는 변수를 선언하여.
+해당 두수의 곱을 출력하는 프로그램을 작성 하시오.
+
+*/
